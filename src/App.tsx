84c14afdf82029b82b23c980d307c87204928f20
jhzrmx <dj.jasper128@gmail.com>
2026-02-11 20:51:15 +0800

    Chore: Removed some tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 function Home() {
   return (
     <div>
-      <h1 className="text-red-600">React Router DOM Setup</h1>
+      <h1>React Router DOM Setup</h1>
     </div>
   );
 }
