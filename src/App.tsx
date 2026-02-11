25f8c73a18e62683dac6a135fb6ba06d7cb14b7d
jhzrmx <dj.jasper128@gmail.com>
2026-02-11 20:23:46 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="">React Router DOM Setup</h1>
+    </div>
+  );
+}
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: Home,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
