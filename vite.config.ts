31a9bf079222d105036886b639e16cb6aeb6ce4c
jhzrmx <dj.jasper128@gmail.com>
2026-02-20 23:50:00 +0800

    Chore: Added option to install as PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,30 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Do It List",
+        short_name: "Todo",
+        theme_color: "#FA5C5C",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   /*
   server: {
     proxy: {
